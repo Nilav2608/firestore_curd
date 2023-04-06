8573fec8a4c0bdd6dfe06edeb839e87f520f92b2
Nilavarasu2608 <knlsu444@gmail.com>
2023-04-06 23:41:10 +0530

    firebase inti

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CURD Operations"),
+        backgroundColor: const Color.fromARGB(255, 153, 0, 255),
+      ),
+    );
   }
 }
